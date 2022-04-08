62961dc3108dbed19684645fde413a8590932ff3
huzaifa621 <99542545+huzaifa621@users.noreply.github.com>
2022-04-08 20:33:38 +0530

    update file

--- src/Counter/Counter.jsx
+++ src/Counter/Counter.jsx
@@ -18,15 +18,26 @@ const Counter = () => {
         setInitialValue(+initialValue*2);
     }
 
-    return (
+    return initialValue%2===0 ? 
+    (
         <div style={{border:"1px solid gray", width:"min-content", padding:"20px", margin: "auto"}}>
             <input type={"text"} value={initialValue} placeholder="Enter initial value here..." onChange={handleChange}/>
-            <h1>{initialValue}</h1>
+            <h1 style={{color:"green"}}>{initialValue}</h1>
+            <button onClick={handleInc}>Increament</button>
+            <button onClick={() => handleDec()}>Decrement</button>
+            <button onClick={() => handleDbl()}>Double</button>
+        </div>
+    ) :
+    (
+        <div style={{border:"1px solid gray", width:"min-content", padding:"20px", margin: "auto"}}>
+            <input type={"text"} value={initialValue} placeholder="Enter initial value here..." onChange={handleChange}/>
+            <h1 style={{color:"red"}}>{initialValue}</h1>
             <button onClick={handleInc}>Increament</button>
             <button onClick={() => handleDec()}>Decrement</button>
             <button onClick={() => handleDbl()}>Double</button>
         </div>
     )
+
 }
 
 export default Counter;
